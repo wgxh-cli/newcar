58c91ddba2b02d9b589d5c2d344f2d2a6385f5b4
Acbox <850625057@qq.com>
2024-05-05 22:53:05 +0800

    chore: update widget3d

--- packages/core/src/widget3d.ts
+++ packages/core/src/widget3d.ts
@@ -1,5 +1,31 @@
+import type { CanvasKit, Paint, Path } from 'canvaskit-wasm'
 import { Widget } from './widget'
+import type { WidgetOptions } from './widget'
+
+export interface Widget3DOptions {
+  x: number
+  y: number
+  z: number
+  style: Widget3DStyle
+}
+
+export interface Widget3DStyle {
+  rotationX: number
+  rotationY: number
+  rotationZ: number
+}
 
 export class Widget3D extends Widget {
   z: number
+  paint: Paint
+  path: Path
+
+  constructor(options: Widget3DOptions) {
+    super(options as WidgetOptions)
+  }
+
+  init(ck: CanvasKit): void {
+    this.paint = new ck.Paint()
+    this.path = new ck.Path()
+  }
 }
